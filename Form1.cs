7adfa49c81c3c240cd93d81004a139e604ca989e
Badea Alamidi <Z1882808@students.niu.edu>
2021-09-14 15:56:22 -0500

    Create Forms layout

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace KozinskiAlamidiAssignment2
         {
             InitializeComponent();
         }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
